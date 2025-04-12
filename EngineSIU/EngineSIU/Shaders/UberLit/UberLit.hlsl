55460e31fabb39d4172255f70e125b0a92af4681
jiwon1304 <143168061+jiwon1304@users.noreply.github.com>
2025-04-12 17:22:57 +0900

    init

---
+++ EngineSIU/EngineSIU/Shaders/UberLit/UberLit.hlsl
@@ -0,0 +1,37 @@
+// UberLit.hlsl
+#include "UberLit/UberLitConstants.hlsli"
+#include "UberLit/UberLitTextures.hlsli"
+
+VS_OUT Uber_VS(VS_IN Input)
+{
+    VS_OUT output;
+#if LIGHTING_MODEL_GOURAUD
+
+#elif LIGHTING_MODEL_LAMBERT
+
+#elif LIGHTING_MODEL_PHONG
+
+#endif
+    return output;
+}
+
+float4 Uber_PS(VS_OUT Input) : SV_TARGET
+{
+    float4 finalPixel = DiffuseTexture.Sample(Sampler, Input.texcoord);
+
+    // Ambient Light의 컬러를 계산
+    finalPixel += float4(AmbientLight.AmbientColor, 1.f) * AmbientLight.Intensity;
+    
+#if LIGHTING_MODEL_GOURAUD
+
+#elif LIGHTING_MODEL_LAMBERT
+    finalPixel += CalculateAmbientLight(...);
+    for(It : PointLights)
+    {
+        finalPixel += CalculatePointLight(...);
+    }
+#elif LIGHTING_MODEL_PHONG
+    // Specular Reflectance
+#endif
+    return finalPixel;
+}
---
+++ EngineSIU/EngineSIU/Shaders/UberLit/UberLitSahdingModels.hlsli
@@ -0,0 +1,8 @@
+// Gouraud Shading
+// Per-Vertex로 값을 구함
+
+// Lambert Shading
+// Per-Pixel로 값을 구함. Diffuse만
+
+// Blinn-Phong Shading
+// Per-Pixel로 값을 구함. Specular + Diffuse
