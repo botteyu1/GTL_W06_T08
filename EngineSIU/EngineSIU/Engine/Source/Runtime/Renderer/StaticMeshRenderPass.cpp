c869f54393b6b6d85e724f651b8fa71785e1d7c4
Xerlocked <xhouse.xman@gmail.com>
2025-04-14 03:59:07 +0900

    UPDATE: 고로드 셰이딩을 위한 VS에서 Constant 버퍼 Set

--- EngineSIU/EngineSIU/Engine/Source/Runtime/Renderer/StaticMeshRenderPass.cpp
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Renderer/StaticMeshRenderPass.cpp
@@ -351,8 +351,10 @@ void FStaticMeshRenderPass::PrepareRenderState() const
     // 상수 버퍼 바인딩 예시
     ID3D11Buffer* PerObjectBuffer = BufferManager->GetConstantBuffer(TEXT("FPerObjectConstantBuffer"));
     ID3D11Buffer* CameraConstantBuffer = BufferManager->GetConstantBuffer(TEXT("FCameraConstantBuffer"));
+    ID3D11Buffer* LightBuffer = BufferManager->GetConstantBuffer(TEXT("FSceneLightBuffer"));
     Graphics->DeviceContext->VSSetConstantBuffers(0, 1, &PerObjectBuffer);
     Graphics->DeviceContext->VSSetConstantBuffers(1, 1, &CameraConstantBuffer);
+    Graphics->DeviceContext->VSSetConstantBuffers(2, 1, &LightBuffer);
 
     TArray<FString> PSBufferKeys = {
                                   TEXT("FCameraConstantBuffer"),
